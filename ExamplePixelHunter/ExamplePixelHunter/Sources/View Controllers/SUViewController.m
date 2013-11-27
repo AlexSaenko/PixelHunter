4615bd2c477f8ae7592316e58d23ae9919c19be3
Aleksander Saenko <aleksander.saenko@sigmaukraine.com>
2013-11-27 19:37:00 +0200

    Added files to index

---
+++ ExamplePixelHunter/ExamplePixelHunter/Sources/View Controllers/SUViewController.m
@@ -0,0 +1,28 @@
+//
+//  SUViewController.m
+//  ExampleCoolTool
+//
+//  Created by Alex Saenko on 11/1/13.
+//  Copyright (c) 2013 Sigma Ukraine. All rights reserved.
+//
+
+#import "SUViewController.h"
+#import "SUPixelHunter.h"
+
+@interface SUViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation SUViewController
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+	SUPixelHunter *coolTool = [[SUPixelHunter alloc] init];
+    [coolTool createWindowForDebugWithImage:self.imageView.image];
+}
+
+@end
---
+++ ExamplePixelHunter/ExamplePixelHunter/Supporting Files/SUAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SUAppDelegate.h
+//  ExampleCoolTool
+//
+//  Created by Alex Saenko on 11/1/13.
+//  Copyright (c) 2013 Sigma Ukraine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SUPixelHunter.h"
+
+@interface SUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
